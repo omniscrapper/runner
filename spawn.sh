f6966f31955e8b837f1749fe5adec01f6206d3bd
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-11-04 20:29:25 +0300

    Update sample spawn script

--- spawn.sh
+++ spawn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-bundle exec ruby -e "require './runner'; Workers::Scrapping.perform_async(11)"
+# Enqueues specified task in sidekiq.
+# The only argument for this script is an ID of task in OmniScrapperHQ database.
+# Example: ./spawn.sh 11
+bundle exec ruby -e "require './runner'; Workers::Scrapping.perform_async($1)"
