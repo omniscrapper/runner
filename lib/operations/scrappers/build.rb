b65681fe70d3001c4f1ece9ad82ceab579e24f69
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-11-14 11:29:11 +0300

    WIP send scrapping metrics

--- lib/operations/scrappers/build.rb
+++ lib/operations/scrappers/build.rb
@@ -6,6 +6,9 @@ module Operations
     class Build
       def call(task)
         OmniScrapper.setup(scrapper_name(task)) do |config|
+          config.scrapping_error_handler = scrapping_error_handler(task)
+          config.scrapping_success_handler = scrapping_success_handler(task)
+
           config.schema task.schema_definition
           config.crawler task.crawler
 
@@ -23,6 +26,18 @@ module Operations
 
       def scrapper_name(task)
         "task_#{task.id}_#{task.site.name}_scrapper".to_sym
+      end
+
+      def scrapping_success_handler(task)
+        -> (uri, ex) do
+          puts "Scrapping error callback! #{uri} #{ex.message}"
+        end
+      end
+
+      def scrapping_error_handler(task)
+        -> (uri, result) do
+          puts "Success! #{uri} #{result}"
+        end
       end
     end
   end
